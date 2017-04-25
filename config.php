323bcf99b53ef9c5a6af762ff5e6b169cf189d0c
gaurav22021996 <gauravmouryaweb@gmail.com>
2017-04-25 17:23:03 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "webcraf1_naveen");
+define("DB_PASSWORD", "Webcraft!@#");
+define("DB_DATABASE", "webcraf1_gangwal_admin");
+ 
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyAefQBT0zz6SPNqVhRgPFARvDLX2EpEP3Y"); // Place your Google API Key
+?>
